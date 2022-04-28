e1cd5b8874e8b99ec0c0e0af7624ce4570375266
nehabht <ne.bhatti2@icloud.com>
2022-04-28 14:55:44 +0200

    add milestone4 fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -234,3 +234,8 @@ un “ok” come risposta, che apparirà dopo 1 secondo. */
 ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
 contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
 “mar” rimangono solo Marco e Martina) */
+
+/* Milestone 5 - opzionale
+● Cancella messaggio: cliccando sul messaggio appare un menu a tendina che
+permette di cancellare il messaggio selezionato
+● Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti */
